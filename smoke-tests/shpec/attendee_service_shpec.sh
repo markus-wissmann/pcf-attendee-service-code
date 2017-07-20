cb4362fbd739ff7e4632ab56e710c920545558f3
Pivotal <pivotal@Pivotals-iMac-5.local>
2017-07-20 16:24:22 -0700

    check whether curl is there

--- smoke-tests/shpec/attendee_service_shpec.sh
+++ smoke-tests/shpec/attendee_service_shpec.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 describe "attendee service"
-  describe "GET /"
+  describe "GET / `which curl`"
     http_result=`curl -s -o /dev/null -H 'Accept: application/json' $ATTENDEE_SERVICE_URL -w "%{http_code},%{content_type}"`
     http_code=`echo $http_result | cut -d, -f1`
     content_type=`echo $http_result | cut -d, -f2`
